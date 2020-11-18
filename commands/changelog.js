78f1457c266d61f9f783956b56034cf7168e3576
ThaddeusKKR <thaddeuskkr@gmail.com>
2020-11-19 01:49:13 +0800

    changelog

--- commands/changelog.js
+++ commands/changelog.js
@@ -14,12 +14,14 @@ module.exports = {
         const embed = new MessageEmbed()
             .setDescription(`Here is the changelog for RavenBot \`${version}\`.`)
             .addField(`Changelog`, "```diff\n" +
+                "- Commit: v22b560 (v117)\n" +
                 "+ added a prefix command\n" +
                 "- removed global prefix after setting server prefix\n" +
                 "+ added this changelog\n" +
                 "+ made it such that userinfo works with user ids as a argument\n" +
                 "+ made a working play command (haha music features coming soon)\n" +
                 "+ changed the default volume to 40%\n" +
+                "+ added a bunch of other music commands\n" +
                 "```")
             .setFooter(`Requested by ${message.author.tag}`)
             .setTimestamp()
