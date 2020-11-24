197f40cfa4132934ca9d0cf97514e1312bd657c8
ThaddeusKKR <thaddeuskkr@gmail.com>
2020-11-24 20:28:13 +0800

    changelog improvements

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require('discord.js')
 const { globalPrefix } = require('../config.json')
 const Keyv = require('keyv')
+const fs = require('fs')
 
 module.exports = {
     name: 'changelog',
@@ -10,23 +11,7 @@ module.exports = {
     modOnly: false,
     ownerOnly: false,
     async execute(message, args) {
-        let version = args[0]
-        if (!args.length) version = 'latest'
-        const embed = new MessageEmbed()
-            .setDescription(`Here is the changelog for RavenBot \`${version}\`.`)
-            .addField(`Changelog`, "```diff\n" +
-                "- Commit: v22b560 (v117)\n" +
-                "+ added a prefix command\n" +
-                "- removed global prefix after setting server prefix\n" +
-                "+ added this changelog\n" +
-                "+ made it such that userinfo works with user ids as a argument\n" +
-                "+ made a working play command (haha music features coming soon)\n" +
-                "+ changed the default volume to 40%\n" +
-                "+ added a bunch of other music commands\n" +
-                "```")
-            .setFooter(`Requested by ${message.author.tag}`)
-            .setTimestamp()
-            .setColor("RANDOM")
-        message.channel.send(embed)
+        const changelog = fs.readFileSync('../changelog.txt')
+        message.channel.send(`Changelog: \`\`\`diff\n${changelog}\n\`\`\``)
     }
 }
