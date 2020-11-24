5b179f5b64d8e8a662601114ad655921f012052b
ThaddeusKKR <thaddeuskkr@gmail.com>
2020-11-25 02:57:53 +0800

    info

---
+++ commands/info.js
@@ -0,0 +1,43 @@
+const { MessageEmbed } = require('discord.js')
+const Keyv = require('keyv')
+
+module.exports = {
+    name: 'info',
+    description: 'Shows information such as requested by for a song in queue.',
+    aliases: [],
+    category: 'music',
+    modOnly: false,
+    ownerOnly: false,
+    async execute(message, args, prefix) {
+        const index = args[0] - 1
+        if (!message.guild.musicData.nowPlaying) {
+            const embed = new MessageEmbed()
+                .setDescription(`There is no song playing right now!`)
+                .setColor("RED")
+            return message.channel.send(embed)
+        }
+        if (message.guild.musicData.queue.length == 0) {
+            const embed = new MessageEmbed()
+                .setDescription(`There are no songs in queue!`)
+                .setColor("RED")
+            return message.channel.send(embed)
+        }
+        if (args[0] < 1 && args[0] >= message.guild.musicData.queue.length) {
+            const errorEmb = new MessageEmbed()
+                .setDescription(`You entered an invalid song number.`)
+                .setColor("RED")
+            return message.channel.send(errorEmb)
+        }
+        const song = message.guild.musicData.queue[index]
+
+        const embed = new MessageEmbed()
+            .setTitle(`Information about song ${args[0]}`)
+            .addField(`Title`, song.title)
+            .addField(`URL`, song.url)
+            .addField(`Duration`, song.duration, true)
+            .addField(`Requested by`, song.requestedBy.toString(), true)
+            .addField(`Position in queue`, index + 1)
+            .setImage(song.thumbnail)
+        return message.channel.send(embed)
+    }
+}
--- commands/queue.js
+++ commands/queue.js
@@ -21,6 +21,7 @@ module.exports = {
                 .setTitle(`Queue for ${message.guild.name}`)
                 .addField(`Now Playing`, `[${message.guild.musicData.nowPlaying.title}](${message.guild.musicData.nowPlaying.url}) - ${mesasge.guild.musicData.nowPlaying.requestedBy.toString()}`)
                 .addField(`# - Song`, `No songs in queue.`)
+                .setColor("PURPLE")
             return message.channel.send(embed)
         }
         if (message.guild.musicData.queue.length == 0) {
@@ -37,17 +38,17 @@ module.exports = {
             .setElementsPerPage(8)
             .formatField(`# - Song`, function(e) {
                 let title;
-                if (e.title.length > 64) {
-                    e.title.slice(64, e.title.length)
+                if (e.title.length > 60) {
+                    e.title.slice(60, e.title.length)
                     title = `${e.title}...`
                 } else {
                     title = e.title
                 }
-                return `**${queueClone.indexOf(e) + 1}**: [${title}](${e.url}) - ${e.requestedBy.tag}`;
+                return `**${queueClone.indexOf(e) + 1}**: [${title}](${e.url})`;
             });
 
 
-        queueEmbed.embed.setColor('PURPLE').setTitle(`Queue for ${message.guild.name}`)
+        queueEmbed.embed.setColor('PURPLE').setTitle(`Queue for ${message.guild.name}`).setFooter(`Use ${prefix}info to see information about a song in queue.`)
         queueEmbed.build();
     }
 }
