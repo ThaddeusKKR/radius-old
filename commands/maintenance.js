c70ea840a67a34b10876175a84c1c5ce2d063609
ThaddeusKKR <thaddeuskkr@gmail.com>
2020-11-26 00:55:13 +0800

    bugfix for maintenance

--- commands/maintenance.js
+++ commands/maintenance.js
@@ -1,6 +1,5 @@
 const { MessageEmbed } = require('discord.js')
 const Keyv = require('keyv')
-const client = require('discord.js')
 
 module.exports = {
     name: 'maintenance',
@@ -19,16 +18,16 @@ module.exports = {
             newState = true
             wordState = "enabled"
             reason = args.join(' ') || "No reason provided."
-            await client.user.setStatus(`DND`)
-            await client.user.setActivity(`maintenance | radius.tk`, {
+            await message.client.user.setStatus(`DND`)
+            await message.client.user.setActivity(`maintenance | radius.tk`, {
                 type: "PLAYING"
             })
         } else if (state == true) {
             newState = false
             wordState = "disabled"
             reason = args.join(' ') || "Turned off by bot owner."
-            await client.user.setStatus(`ONLINE`)
-            await client.user.setActivity(`rd!h | radius.tk`, {
+            await message.client.user.setStatus(`ONLINE`)
+            await message.client.user.setActivity(`rd!h | radius.tk`, {
                 type: "STREAMING",
                 url: "https://twitch.tv/thaddeuskkr"
             })
