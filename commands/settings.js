7bf28bbdbffcb8c32aeda08f1f642798f8af7fc3
ThaddeusKKR <thaddeuskkr@gmail.com>
2020-11-27 02:19:00 +0800

    removed welcome msg

--- commands/settings.js
+++ commands/settings.js
@@ -11,6 +11,12 @@ module.exports = {
     async execute(message, args, prefix) {
         const db = new Keyv(process.env.DATABASE_URL, { namespace: 'guild-settings' })
         const guildSettings = await db.get(message.guild.id)
+        if (!guildSettings) {
+            const embed = new MessageEmbed()
+                .setDescription(`Guild settings currently not available.`)
+                .setColor("RED")
+            return message.channel.send(embed)
+        }
         const setting = args[0]
         let value;
         if (args.length < 2) {
